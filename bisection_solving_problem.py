6b7b8a9ea84839cdcc1cba18916e7b47a2e5b2ca
GutierrezJon <gutierrezjon87@gmail.com>
2023-06-14 00:54:59 -0600

    Modified python script. Fixed bugs and added output file.

--- bisection_solving_problem.py
+++ bisection_solving_problem.py
@@ -7,15 +7,20 @@
 '''
 import numpy as np
 import time
+import functools
 
 def func_timer(func):
     '''Decorator Function Timer'''
-    def wrapper(*args,**kwargs):
-        t = time.process_time()
-        func(*args,**kwargs)
-        elapsed_time = time.process_time() - t
-        print(f"Time elapsed: {elapsed_time}.")
-    return wrapper
+    #Functools.wraps preserves functions documentation/metadata
+    @functools.wraps(func) 
+    def func_wrapper(*args,**kwargs):
+        t = time.time()
+        func_results = func(*args,**kwargs)
+        #Get total time of function call. Round to 9 decimal places
+        elapsed_time = np.round(time.time() - t, 9) 
+        print(f"Time elapsed: {elapsed_time} seconds.")
+        return elapsed_time, func_results 
+    return func_wrapper
 
 
 @func_timer
@@ -27,19 +32,20 @@ def bisection(func,a,b,tol,max_iter):
         tol:        <double>    Tolerance for convergence
         max_iter:   <int>       Max number of iterations for method
     """
+    #time.sleep(2.5)
     for i in range(max_iter):
         mid = (a+b)/2
         #print(mid)
         if func(mid) == 0 or np.abs((b-a)/2) < tol:
-            print(f"Root = {mid}. Converged in {i} iterations.")
-            return mid
+            print(f"Root = {mid}. Converged in {i+1} iterations.")
+            return mid, i+1
         
         if np.sign(func(mid)) == np.sign(func(a)):
             a = mid
         else:
             b = mid
-    print(f"Max iterations Reached. Midpoint value = {mid}") 
-    return None
+    print(f"Max iterations reached. Ending script. Current midpoint value = {mid}.") 
+    exit()
 
 
 def main():
@@ -50,10 +56,15 @@ def main():
     a = 0.0
     b = 10.0
     tol = 10**(-10)
-    max_iter = 100
+    max_iter = 50
 
     #Run Method
-    root = bisection(f,a,b,tol,max_iter)
+    elapsed_time, (root, curr_iter)  = bisection(f,a,b,tol,max_iter)
+
+    with open("output.txt", "w") as g:
+        g.write(f"Solution: m = {root} for a tolerance of {tol}. \n Converged \
+                in {curr_iter} iterations.\n Time elapsed = {elapsed_time} seconds.")
+
 
 if __name__ == "__main__":
     main()
